cce55b51d42b886b156fac8fdc0d39823ede6860
VicentCodes <vicentcodes@gmail.com>
2025-06-27 22:35:44 -0600

    change port default

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const app = require('./src/app');
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 19130;
 
 http.createServer(app).listen(PORT, () => {
   console.log(`API escuchando en puerto ${PORT}`);
